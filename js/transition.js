ab320d4f39ff23719c3ea73a94f3af18f2695ec7
amasc4 <19amustefa@ecfs.org>
2018-01-06 22:48:51 -0500

    Add files via upload

---
+++ js/transition.js
@@ -0,0 +1,7 @@
+src="https://code.jquery.com/jquery-3.2.1.js"
+integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+crossorigin="anonymous"
+  
+setTimeout(function(){ 
+    window.location.assign("home.html"); 
+}, 3000);
